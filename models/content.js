905419da80d114fe2c9266dcfd6c7794c47e462d
Peyton C <peyton.castill0@icloud.com>
2022-10-04 12:29:12 -0400

    Adding the contnet model that will belong to users

--- models/content.js
+++ models/content.js
@@ -1,2 +1,39 @@
 // content belongs to user
 // a user can have many contents but a content can only have on user
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Content extends Model {}
+
+Content.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'content',
+    }
+)
+module.exports = Content
